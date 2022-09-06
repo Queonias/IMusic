74e6ffdef85c96180c6c37fb932134de46dc2654
queones <queones@gmail.com>
2022-09-05 23:52:48 -0300

    Refatorando o requisito 5

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -2,11 +2,47 @@ import React, { Component } from 'react';
 import Header from '../Header';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.validateInput = this.validateInput.bind(this);
+
+    this.state = {
+      music: '',
+      disabled: true,
+    };
+  }
+
+  validateInput({ target }) {
+    const { value } = target;
+    this.setState({ music: value });
+    if (value.length >= 2) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  }
+
   render() {
+    const { music, disabled } = this.state;
     return (
       <div data-testid="page-search">
         <p>Pesquisa</p>
         <Header />
+        <form>
+          <label htmlFor="search-artist-input">
+            <input
+              type="text"
+              name="name"
+              id=""
+              data-testid="search-artist-input"
+              value={ music }
+              onChange={ this.validateInput }
+            />
+          </label>
+          <button type="button" data-testid="search-artist-button" disabled={ disabled }>
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
