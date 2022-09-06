a6537853389e8041aa868d2b21ceb00b12d5957b
queones <queones@gmail.com>
2022-09-05 23:25:14 -0300

    Refatorando o requisito 3

--- src/components/pages/Favorites.jsx
+++ src/components/pages/Favorites.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../Header';
 
 class Favorites extends Component {
   render() {
     return (
       <div data-testid="page-favorites">
         <p>Favoritos</p>
+        <Header />
       </div>
     );
   }
--- src/components/pages/ProfileEdit.jsx
+++ src/components/pages/ProfileEdit.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../Header';
 
 class ProfileEdit extends Component {
   render() {
     return (
       <div data-testid="page-profile-edit">
         <p>Editar Perfil</p>
+        <Header />
       </div>
     );
   }
