f29b2872fdad1f08e8fe0ece5b38086aa9dd191d
queones <queones@gmail.com>
2022-09-14 18:49:28 -0300

    Requisito 13

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Header';
+import { getUser } from '../../services/userAPI';
+import Loading from '../Loading';
 
 class Profile extends Component {
+  state = {
+    loading: false,
+    NameUser: [],
+    showScreen: false,
+  };
+
+  componentDidMount() {
+    this.userScreen();
+  }
+
+  userScreen = () => {
+    this.setState({ loading: true }, async () => {
+      const user = await getUser();
+      this.setState({ NameUser: user, loading: false, showScreen: true });
+    });
+  };
+
   render() {
+    const { loading, showScreen, NameUser } = this.state;
     return (
       <div data-testid="page-profile">
-        <p>Perfil</p>
         <Header />
+        {loading && <Loading />}
+        {showScreen && (
+          <div>
+            <span>{NameUser.name}</span>
+            <span>{NameUser.description}</span>
+            <span>{NameUser.email}</span>
+            <img
+              src={ `${NameUser.image}` }
+              alt="Foto do usuário"
+              data-testid="profile-image"
+            />
+          </div>
+        )}
+        <Link to="/profile/edit">Editar perfil</Link>
       </div>
     );
   }
